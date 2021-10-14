e1a5ffb972bd9764c97574ff455ee54434c3aa7e
lhs961021 <lhs961021@naver.com>
2021-10-14 21:21:53 +0900

    practice2

---
+++ Project2/practice2.c
@@ -0,0 +1,110 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#define pyeong 3.3058
+
+
+int main(void) {
+	/*
+
+	//문제 1
+	float num1 = 0;
+
+	printf("실수를 입력하시오: ");
+	scanf("%f", &num1);
+	printf("실수형식으로는 %f입니다\n", num1);
+	printf("지수형식으로는 %e입니다\n", num1);
+
+	//문제 2
+	int num2 = 0;
+
+	printf("16진수 정수를 입력하시오: ");
+	scanf("%x", &num2);
+	printf("8진수로는 %#o 입니다\n", num2);
+	printf("10진수로는 %d 입니다\n", num2);
+	printf("16진수로는 %#x 입니다\n", num2);
+
+	//문제 3
+	int x = 10;
+	int y = 20;
+	int a = 0;
+
+	printf("x=%d y=%d\n", x, y);
+	a = x;
+	x = y;
+	y = a;
+	printf("x=%d y=%d\n", x, y);
+
+	
+	//문제 4
+	double length= 0;
+	double width = 0;
+	double height = 0;
+	double volume = 0;
+	
+
+	printf("상자의 가로 세로 높이를 한번에 입력: ");
+	scanf("%lf %lf %lf", &width, &length, &height);
+	volume = length * width * height;
+	printf("상자의 부피는 %lf 입니다", volume);
+	
+
+	//문제 5
+	int num3 = 0;
+
+	printf("평을 입력하세요: ");
+	scanf("%d", &num3);
+
+	double pyeongbang = 0;
+	pyeongbang = pyeong * num3;
+
+	printf("%lf평방미터입니다", pyeongbang);
+	
+
+	//문제 6
+	double num4 = 3.32e-3;
+	double num5 = 9.76e-8;
+
+	printf("%lf", num4 + num5);
+	
+
+	//문제 7
+	double m = 0;
+	double v = 0;
+	double E = 0;
+	
+	printf("질량(kg): ");
+	scanf("%lf", &m);
+	printf("속도(m/s): ");
+	scanf("%lf", &v);
+	E = m * v * v / 2.0;
+	printf("운동에너지(J): %lf", E);
+	
+
+	//문제 8
+	int as = 0;
+
+	printf("아스키 코드값을 입력하시오: ");
+	scanf("%d", &as);
+	printf("문자: %c", as);
+	
+
+	//문제 9
+	char ch = 'a';
+	int v = ch;
+
+	printf("%c %c %c", v+1,v+2,v+3);
+	
+
+	//문제 10
+	printf("\a화재가 발생하였습니다.\a");
+	*/
+
+	//문제 11
+	printf("\"ASCII code\" , \'A\' , \'B\' , \'C\'\n");
+	printf("\\t \\a \\n");
+	
+
+
+	return 0;
+}
