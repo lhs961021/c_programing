d5dff99b0c76e34077e960efaa1145b1ddb805c2
lhs961021 <lhs961021@naver.com>
2021-10-17 21:02:39 +0900

    practice 1008

--- Project3/1003_practice.c
+++ Project3/1003_practice.c
@@ -3,4 +3,57 @@
 
 int main(void) {
 	
+	/*
+	//문제 1
+	int num1 = 0;
+	int num2 = 0;
+
+	printf("2개의 정수를 입력하시오: ");
+	scanf("%d %d", &num1, &num2);
+
+	printf("몫:%d 나머지:%d", num1/num2, num1%num2);
+
+	
+	//문제 2
+
+	double num3 = 0;
+	double num4 = 0;
+
+	printf("실수를 입력하시오: ");
+	scanf("%lf %lf", &num3, &num4);
+
+	printf("%lf %lf %lf %lf", num3 + num4, num3 - num4, num3 * num4, num3 / num4);
+
+	
+
+	//문제 3
+
+	int num5, num6, num7, number;
+
+	printf("3개의 정수를 입력하시오: ");
+	scanf("%d %d %d", &num5, &num6, &num7);
+
+	if (num5 > num6) {
+		number = num5;
+	}
+	else {
+		number = num6;
+	}
+
+	if (number > num7) {
+		printf("최대값: %d", number);
+	}
+	else {
+		printf("최대값: %d", num7);
+	}
+
+	*/
+
+	//문제 4
+
+
+
+
+	return 0;
+
 }
